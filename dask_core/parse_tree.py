e4c9210c72530fc41abaabc8bbf8c72e16c3ace7
xheah <adencheah07@gmail.com>
2026-01-13 11:50:41 +0800

    parse tree

--- dask_core/parse_tree.py
+++ dask_core/parse_tree.py
@@ -1,3 +1,23 @@
 """
 ParseTree class + methods (build, evaluate, print)
 """
+from dask_core.tree_node import TreeNode
+class ParseTree:
+    def __init__(self, root=None):
+        self.root = root
+    
+    def evaluate(self, evaluator, context):
+        if self.root is None:
+            return None
+        
+        return evaluator.eval_node(self.root, context)
+    
+    def print_rotated(self, node: TreeNode = None, level: int = 0):
+        if node is None:
+            node = self.root
+
+        if node.right:
+            self.print_rotated(node.right, level + 1)
+        print("    " * level + str(node.value))
+        if node.left:
+            self.print_rotated(node.left, level + 1)
