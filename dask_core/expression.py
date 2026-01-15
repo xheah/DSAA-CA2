4fd34b7343fc60df87ad7eba054e0ddbbb2e6b44
xheah <adencheah07@gmail.com>
2026-01-15 10:57:05 +0800

    temp

--- dask_core/expression.py
+++ dask_core/expression.py
@@ -3,16 +3,16 @@ DaskExpression class (var name, raw expr, value, tree)
 """
 from dask_core.parser import ExpressionParser
 from dask_core.evaluator import Evaluator
+from dask_core.parse_tree import ParseTree
 class DaskExpression:
     def __init__(self, var_name: str, expr: str):
         self.name = var_name
         self.expression = expr
-        self.parse_tree = None
+        self.parse_tree: ParseTree | None = None
         self.value = None
         self.build_tree()
+        self.parse_tree.evaluate
 
     def build_tree(self, parser = ExpressionParser()):
         self.parse_tree = parser.parse(self.expression)
     
-    def evaluate(self, evaluator = Evaluator()):
-        pass
