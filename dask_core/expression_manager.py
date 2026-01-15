42db706eb208867e9a7699dc711447b3fa28e45e
zuhaooo <MZUHAO.24@ichat.sp.edu.sg>
2026-01-15 09:24:55 +0800

    finished part 1

--- dask_core/expression_manager.py
+++ dask_core/expression_manager.py
@@ -2,9 +2,65 @@
 Manages all expressions (add, modify, lookup, sort)
 """
 from dask_core.parser import ExpressionParser
+import re
 class ExpressionManager:
     def __init__(self):
         self.expressions = {} # dict[str, DaskExpression]
         self.parser = ExpressionParser()
 
-    
+    def add_expression(self, var_name: str, expression_str: str):
+        """
+        Docstring for add_expression
+        
+        :param self: Description
+        :param var_name: Description
+        :type var_name: str
+        :param expression_str: Description
+        :type expression_str: str
+        """
+        # expression = self.parser.parse(var_name, expression_str)
+        self.expressions[var_name] = expression_str
+        print(self.expressions)
+
+    def validate_expression(self, expression:str):
+        valid_operators = {'+', '-', '*', '/', '**', '++', '//'}
+        allowed_chars = set('0123456789+-*/()=')
+
+        if '=' not in expression:
+            return "*Missing '=' sign in expression. Please re enter the expression*", False, '', ''
+        
+        if expression.count('=') > 1:
+            return "*Multiple '=' signs in expression. Please re enter the expression*", False, '', ''
+
+        if expression.count('(') != expression.count(')'):
+            return "*Mismatched parentheses in expression. Please re enter the expression*", False, '', ''
+        
+        if re.search(r'\(\s*\)', expression):
+            return "*Empty parentheses in expression. Please re enter the expression*", False, '', ''
+            
+        for char in expression:
+            if char not in valid_operators and char not in allowed_chars and not char.isalpha():
+                return f"*Invalid character '{char}' in expression. Please re enter the expression*", False, '', ''
+            
+
+        name, expr = expression.split('=', 1)
+        name = name.strip()
+        expr = expr.strip()
+
+        if not re.match(r'^[a-zA-Z_]+$', name):
+            return "*Invalid variable name. Please re enter the expression*", False, '', ''
+        
+        if re.search(r'[\+\-*/^][\s\)]*$', expr):
+            return "*Expression cannot end with an operator. Please re enter the expression*", False, '', ''
+        
+        if re.search(r'^[+\-*/^]\s*', expr):
+            return "*Expression cannot start with an operator. Please re enter the expression*", False, '', ''
+        
+        if expr == '':
+            return "*Expression cannot be empty. Please re enter the expression*", False, '', ''
+
+        return '', True, name, expr
+        
+
+    def get_expression(self):
+        pass
--- ui/menu.py
+++ ui/menu.py
@@ -1,3 +1,7 @@
+from dask_core.expression_manager import ExpressionManager
+
+EM = ExpressionManager()
+
 class Menu:
     def __init__(self):
         self.option_display = ""
@@ -13,6 +17,46 @@ class Menu:
     
 
     def run_menu(self):
+        print('''
+*********************************************************
+* ST1507 DSAA: DASK Expression Evaluator                *
+*-------------------------------------------------------*
+*                                                       *
+*  - Done by: Mo Zuhao(2415646) & Aden Cheah(24151815)  *
+*  - Class DAAA/2A/21                                   *
+*                                                       *
+*********************************************************
+              
+
+''')
         while True:
-            user_choice = input(self.option_display)
+            user_choice = input(self.option_display).strip()
+            while user_choice not in ['1','2','3','4','5','6']:
+                user_choice = input(f'\n*PLEASE ENTER A VALID NUMBER*\n{self.option_display}')
+            
+            match user_choice:
+                case '1':
+                    expression = input('Enter the DASK expression you want to add/modify: \nFor example, a=(1+2)\n')
+                    message, result,name,expr = EM.validate_expression(expression)
+                    while True:
+                        if result == False:
+                            expression = input(f'\n{message}: ')
+                            message,result,name,expr = EM.validate_expression(expression)
+                        elif result == True:
+                            print('gonna add into dictionary')
+                            EM.add_expression(name, expr)
+                            break
+                case '2':
+                    print('''CURRENT EXPRESSIONS:
+********************''')
+                    
+                case '3':
+                    print('filler3')
+                case '4':
+                    print('filler4')
+                case '5':
+                    print('filler5')
+                case '6':
+                    break
+        print('\nBye, thanks for using ST1507 DSAA DASK Expression Evaluator')
             
