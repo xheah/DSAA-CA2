7f3d93f3447fc1558b5933f8d020c892a0f25e2f
xheah <adencheah07@gmail.com>
2026-01-17 12:05:28 +0800

    Outlined description for cost analysis:

---
+++ features/cost_analysis.py
@@ -0,0 +1,8 @@
+"""
+Cost Analysis for changes between original tree and optimised tree
+Metrics:
+    node_count: total nodes
+    operator_count: internal operator nodes
+    leaf_count: numbers + variables
+    tree_height / max_depth
+"""
